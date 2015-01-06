1be689dfe17c5eef06339f260672ebd4d52d9eeb
jonbloom <github@jon.tw>
2015-01-06 13:16:07 -0500

    accesslog

--- room-status.php
+++ room-status.php
@@ -3,6 +3,28 @@ date_default_timezone_set('America/Detroit');
 include 'inc/config.php';
 $db = new mysqli($db_host, $db_user, $db_pass, $db_database);
 
+function checkIP(){
+    global $db;
+    $ip = $_SERVER['REMOTE_ADDR'];
+    $ua = $_SERVER['HTTP_USER_AGENT'];
+    $db->query("INSERT INTO `access_log`
+            (accessid, system, ip, useragent, timestamp) VALUES
+            (NULL, 'room-status', '$ip', '$ua', SYSDATE())");
+    $ip = explode(".",$ip);
+    if (!(
+        ($ip[0] == "::1") ||
+        ($ip[0] == "148" && $ip[1] == "61") ||
+        ($ip[0] == "35" && $ip[1] == "40") ||
+        ($ip[0] == "207" && $ip[1] == "72" &&
+            ($ip[2] >= 160 && $ip[2] <= 191)
+        ))
+
+    ){
+        die();
+    }
+}
+checkIP();
+
 function getRoomTrafficByDatabaseID($id){
 	global $db;
 	$query = "
